3794b1ecf424228c478c42e523fe8205fc0e957a
Chopra <sidharth.chopra@ucdenver.edu>
2023-05-05 11:52:24 -0600

    updated code with incorrect posture

--- train_model.py
+++ train_model.py
@@ -1,19 +1,19 @@
 from keras.models import Sequential
 from keras.layers import Dense,Dropout
+from keras.regularizers import l2
 
 # Define the neural network architecture
 model = Sequential()
-model.add(Dense(32, activation='relu', input_dim=2))
-model.add(Dropout(0.5))
-model.add(Dense(16, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01), input_dim=2))
+model.add(Dense(16, activation='relu', kernel_regularizer=l2(0.01)))
+model.add(Dense(8, activation='relu', kernel_regularizer=l2(0.01)))
 model.add(Dense(1, activation='sigmoid'))
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Train the model on the training data
-history =model.fit(train_angles, train_labels, validation_data=(val_angles, val_labels), epochs=20, batch_size=5)
+history =model.fit(train_angles, train_labels, validation_data=(val_angles, val_labels), epochs=20, batch_size=10)
 
 # Evaluate the model on the test data
 loss, accuracy = model.evaluate(test_angles, test_labels)
